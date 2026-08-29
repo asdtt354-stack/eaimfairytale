dd57ea0cbc56fc54e5bfbf5265a5829645725329
asdtt354-stack <asdtt354@gmail.com>
2026-08-30 00:35:20 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='muni-story-v5';
+const CACHE_NAME='muni-story-v6';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event=>{
